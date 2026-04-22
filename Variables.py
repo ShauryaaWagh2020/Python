b216132c52f351155ea0fc34d8edd5a0314ba99d
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-22 14:10:15 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,17 @@
+print("Variables\n")
+print("User Defined Variables\n")
+a = 6
+print(a)
+b = 10
+print(b,"\n")
+print("Pre-Defined Variables \n")
+Int = 3
+print(Int)
+Float = 16.25
+print(Float)
+Boolean = True
+print(Boolean)
+String = ("Good Morning")
+print(String,"\n")
+Subject = input("Enter the Subject:")
+print = Subject
