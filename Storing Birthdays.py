4469873860d6fbbace865996d053592dd588a1b3
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-22 16:41:27 +0530

    Add files via upload

---
+++ Storing Birthdays.py
@@ -0,0 +1,23 @@
+print("Storing Birthdays \n")
+
+print("Storing birthdays of Raj's 5 Friends. \n")
+
+# Friend 1: Rahul
+a = ("15 November 2013")
+print("Rahul's Birthday:",a,"\n")
+
+# Friend 2: Nirav
+b = ("20 March 2012")
+print("Nirav's Birthday:",b,"\n")
+
+# Friend 3: Kiran
+c = ("20 August 2013")
+print("Kiran's Birthday:",c,"\n")
+
+# Friend 4: Parth
+d = ("13 September 2015")
+print("Parth's Birthday:",d,"\n")
+
+# Friend 5 Rohan
+e = ("4 December 2012")
+print("Rohan's Birthday:",e,"\n")
