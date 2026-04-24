4c1dd968938665e2d2dd6563bd7d19c92b17925e
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-24 12:39:10 +0530

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,19 @@
+a = 7
+print(a)
+print(type(a))
+
+y = "Happy"
+print(y)
+print(type(y))
+
+d = 12.5
+print(d)
+print(type(d))
+
+e = True
+print(e)
+print(type(e))
+
+h = 'c'
+print(h)
+print(type(h))
