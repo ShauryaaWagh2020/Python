33a9082cb1a0470dd14abe11bf8c78fe381d75f2
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-27 13:51:32 +0530

    Add files via upload

---
+++ Understanding Variables.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x = 9
+y = "Ria"
+
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
