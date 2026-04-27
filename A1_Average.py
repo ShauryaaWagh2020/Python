8bbceaafebd204d567633c7253a46317f15edb70
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-27 12:30:06 +0530

    Add files via upload

---
+++ A1_Average.py
@@ -0,0 +1,11 @@
+Tree1= 38
+Tree2= 67
+Tree3= 85
+Tree4= 91
+Tree5= 72 
+
+sum= (Tree1 + Tree2 + Tree3 + Tree4 + Tree5)
+print("The sum of 5 trees:",sum)
+
+average= (sum/5)
+print("The average of 5 trees:",average)
