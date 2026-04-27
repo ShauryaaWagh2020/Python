aa00fc98eb4ddef5fa2c3da486eb68c9479c9963
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-27 09:28:41 +0530

    Add files via upload

---
+++ Congratulations_message.py
@@ -0,0 +1,15 @@
+# Congratulations Message
+
+print("Congratulations Message\n")
+
+# Input the Message in Lowercase
+
+Message = "congratulations"
+
+# Converting Message in Uppercase
+
+upper_Message = (Message.upper())
+
+# Printing Message in Uppercase
+
+print(Message.upper())
