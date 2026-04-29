983bd2685e5d3cec16a02162bce646d65a6c4e98
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-29 20:57:16 +0530

    Add files via upload

---
+++ Summer Times.py
@@ -0,0 +1,8 @@
+temp = (input("Please Enter the temperature in °C: "))
+
+if temp >= "30°C":
+    print(temp,"is the appropriate weather to wear light and soft clothes.")
+
+else :
+    temp <= "25°C"
+    print(temp,"is not the appropriate weather to wear light clothes.Better to keep Jacket for now.")
