5d8ca934cbc15be405f552e92692123b7869ed05
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-04-30 13:09:57 +0530

    Add files via upload

---
+++ A2_Divisible_numbers.py
@@ -0,0 +1,8 @@
+Numerator = int(input("Enter the Numerator: "))
+Denominator = int(input("Enter the Denominator: "))
+
+if Numerator % Denominator==0:
+    print(Numerator,"is Divisible by",Denominator)
+
+else:
+    print(Numerator,"is not Divisible by",Denominator)
---
+++ A3_Mean_.py
@@ -0,0 +1,13 @@
+mean1 = 38
+correct_num = 56
+wrong_num = 36
+total_num = 40
+
+sum = mean1*total_num
+print("Sum of 40 Number: ",sum)
+
+num2 = sum-((wrong_num)-(correct_num))
+print("sum-((wrong_num)-(correct_num))",num2)
+
+mean2 = num2/total_num
+print(mean2)
