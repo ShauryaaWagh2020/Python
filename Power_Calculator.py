ec70668fbfe28b0927d578c54ff9b4b51f2ab47a
ShauryaaWagh2020 <shashikant2020@gmail.com>
2026-05-06 14:31:24 +0530

    Add files via upload

---
+++ Power_Calculator.py
@@ -0,0 +1,7 @@
+base = int(input("Enter the Base Number: "))
+exponent = int(input("Enter the Exponent: "))
+
+for b in range(exponent):
+    result = base**exponent
+
+print(base,"raise to power of",exponent,"is",result,".")
